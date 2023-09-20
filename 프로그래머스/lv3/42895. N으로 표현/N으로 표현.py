da1b3edd014cb7d42c9daeb84839d8ae2aa143ae
Jaehoon Jung <127265884+J-A-Y2@users.noreply.github.com>
2023-09-20 12:12:27 +0900

    [level 3] Title: N으로 표현, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,24 @@
+def solution(N, number):
+    if N == number:
+        return 1
+
+    dp = [set() for _ in range(8)]
+    dp[0].add(N)
+
+    for i in range(1, 8):
+        dp[i].add(int(str(N) * (i + 1)))
+
+        for j in range(i):
+            for op1 in dp[j]:
+                for op2 in dp[i - j - 1]:
+                    dp[i].add(op1 + op2)
+                    dp[i].add(op1 - op2)
+                    dp[i].add(op1 * op2)
+
+                    if op2 != 0:
+                        dp[i].add(op1 // op2)
+
+        if number in dp[i]:
+            return i + 1
+
+    return -1
