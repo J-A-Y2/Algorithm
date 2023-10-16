ad02eb0bd03884c94de83aede2df9e3058eb0f84
Jaehoon Jung <wjdwogns120523@gmail.com>
2023-10-16 22:57:47 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,8 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    
+    for width in range(3, int(total ** 0.5) + 1):
+        if total % width == 0:
+            height = total // width
+            if (width - 2) * (height - 2) == yellow:
+                return [height, width]  # height와 width를 바꾸어 반환
