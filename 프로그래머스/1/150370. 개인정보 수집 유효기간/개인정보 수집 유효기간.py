2b46157956fcbc61f0dad5560f531d1b703d638a
Jaehoon Jung <wjdwogns120523@gmail.com>
2023-10-24 10:37:17 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.15 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,38 +1,19 @@
 def solution(today, terms, privacies):
+    d ={}
     answer = []
-    time_dict = dict()
-    year, month, day = int(today[0:4]), int(today[5:7]), int(today[8:])
+    today_lst = list(map(int,today.split('.'))) # 오늘 날짜 리스트로 변환
     
-    for term in terms : 
-        case = term[0]
-        time_dict[case] = int(term[2:])
-        # 딕셔너리로 약관 종류에 따른 기간 구분합니다
+    for term in terms: # 약관종류와 개월수 딕셔너리로 저장
+        n, m = term.split()
+        d[n] = int(m)*31 # 한 달 일 수 곱해줌
     
     for i in range(len(privacies)):
-        date, case = privacies[i].split()
-        p_year, p_month, p_day = int(privacies[i][0:4]), int(privacies[i][5:7]), int(privacies[i][8:10])
-        
-        p_month += time_dict[case]
-        
-        # if p_month > 12 : print(p_year,p_month, p_day, case)
-        while p_month > 12 : 
-            p_month -= 12
-            p_year +=1
-            
-            
-        if p_year > year :
-            continue
-            
-        elif p_year == year :
-            if p_month > month :
-                continue
-                
-            elif p_month == month :
-                if p_day > day :
-                    continue
-                    
-        answer.append(i+1)
-        # 배열사용을 위해 i는 0부터 시작하므로 정답에 +1 하여 추가합니다
-        # print(p_year,p_month,p_day,case)
-    
+        date, s = privacies[i].split()
+        date_lst = list(map(int, date.split('.'))) # 수집일자 리스트로 변환
+        year = (today_lst[0] - date_lst[0])*372 # 연도 차이에 일 수 곱해주기
+        month = (today_lst[1] - date_lst[1])*31 # 달 수 차이에 일 수 곱해주기
+        day = today_lst[2] - date_lst[2]
+        total = year+month+day
+        if d[s] <= total:
+            answer.append(i+1)
     return answer
